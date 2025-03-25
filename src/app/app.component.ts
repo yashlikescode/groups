0eb8106a5852da2fd363dc6bbf340b16a0d202ec
Yashasvi <kmryashasvi@gmail.com>
2025-03-25 23:28:04 +0530

    last commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NgFor,NgIf } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +9,7 @@ import { NgFor,NgIf } from '@angular/common';
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  jsonData = [
+  jsonData: { groupName: string; templates: string[] }[] = [
     {
       groupName: 'Group 1',
       templates: ['Template 1A', 'Template 1B', 'Template 1C'],
@@ -24,20 +24,38 @@ export class AppComponent {
     },
   ];
 
-  activeGroup: string | null = null;
+  activeGroup: string = '';
 
   selectedTemplates: { [key: string]: boolean } = {};
+  selectedGroup:string = '';
 
+  toggable: boolean = true;
+  disableToggle() {
+    this.toggable = false;
+  }
+  enableToggle() {
+    this.toggable = true;
+  }
   toggleGroup(group: any) {
     const isActive = this.activeGroup === group.groupName;
-    this.activeGroup = isActive ? null : group.groupName;
-
-    // Toggle all templates of the group
-    group.templates.forEach((template: string) => {
-      this.selectedTemplates[template] = !isActive;
+    if (this.toggable) {
+      this.activeGroup = isActive ? null : group.groupName;
+    }
+  }
+  groupClicked(selectedGroup: any, event: any) {
+    this.jsonData.forEach((group) => {
+      group.templates.forEach((template) => {
+        if (selectedGroup.groupName === group.groupName) {
+          this.selectedTemplates[template] = event.target.checked;
+          if(event.target.checked){
+            this.selectedGroup = selectedGroup.groupName;
+          }
+        } else {
+          this.selectedTemplates[template] = false;
+        }
+      });
     });
   }
-
   isGroupActive(groupName: string): boolean {
     return this.activeGroup === groupName;
   }
@@ -48,5 +66,8 @@ export class AppComponent {
 
   isChecked(template: string): boolean {
     return this.selectedTemplates[template] || false;
+  }
+  isCheckedGroup(group:any):boolean{
+    return group.groupName === this.selectedGroup;
   }
 }
